ee9af4c526cb7b686b58832216e02a76d82886f1
Michael Jones <mykejonez53@gmail.com>
2018-03-24 16:53:15 -0700

    added Customer and Movie tabs

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        Customer[] customers = {
+            new Customer{Name = "John Smith", Id = 1},
+            new Customer{Name = "mary Williams", Id = 2}
+        };
+        // GET: Customers
+        public ActionResult Index(){
+
+            return View(customers);
+        }
+
+
+        public ActionResult Details(int id){
+
+            if (id <= 0 || id > customers.Length){
+                return HttpNotFound();
+            }
+
+            return View(customers[id-1]);
+        }
+}
+
+    
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Random()
-        {
-            var movie = new Movie(){ Name = "Shrek!"};
-            return View(movie);
+        public ActionResult Index(){
+            Movie[] movies = {
+                new Movie(){Name = "Shrek!"},
+                new Movie(){Name = "Wall-e"},
+                new Movie(){Name = "Some Other Movie"}
+            };
+            
+           
+            
+            return View(movies);
         }
     }
 }
